9981e501243a241750417ff1172d027ea70099b4
auilk <abdelghafour.lbaz01@gmail.com>
2025-06-03 14:23:10 +0100

    Set up React project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App()
+{
+  return(
+    <div className="w-dvw h-dvh flex justify-center items-center">
+      <h1 className="text-9xl text-black font-bold">The Paper Wall</h1>
+    </div>
+  );
+}
+
+export default App;
